f2ec81d3d3acc9435b67eb716a1256518bcd8785
davej23 <33763255+davej23@users.noreply.github.com>
2022-02-05 00:22:32 +0000

    Delete nextcloud.sh

--- nextcloud.sh
+++
@@ -1,7 +0,0 @@
-docker run -d \
-    -v /nextcloud/nextcloud:/var/www/html \
-    -v /nextcloud/apps:/var/www/html/custom_apps \
-    -v /nextcloud/config:/var/www/html/config \
-    -v /nextcloud/data:/var/www/html/data \
-    -p 80:80 \
-    nextcloud
