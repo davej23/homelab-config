06bf4f6b70894c43f795b4e72da5972a670f5e11
David John <dt-john@outlook.com>
2023-03-16 12:17:23 +0000

    Fixed script

--- nextcloud/remove_outdated_backups.py
+++ nextcloud/remove_outdated_backups.py
@@ -1,10 +1,11 @@
 # Remove Nextcloud backups older than N days
+# Backup folders are named `backup_YYYYMMDD`
 
 import os
-import time
+from datetime import datetime
 
 BACKUP_PATH = '/mnt/storage/'  # Example
-LOG_FILE = '/mnt/storage/log.txt'  # Example
+LOG_FILE = BACKUP_PATH + 'log.txt'  # Example
 N_DAYS = 3
 
 if not os.path.exists(LOG_FILE):
@@ -15,17 +16,19 @@ with open(LOG_FILE, 'a', encoding='utf-8') as f:
 
 for backup_folder in os.listdir(BACKUP_PATH):
     if os.path.isdir(BACKUP_PATH + backup_folder):
-        backup_stats = os.stat(BACKUP_PATH + backup_folder)
-        modified_time = backup_stats.st_mtime
 
-        time_since_modified_in_seconds = (time.time() - modified_time) / 1000.0
-        time_since_modified_in_days = time_since_modified_in_seconds / (60 * 60 * 24)
+        backup_date_str = backup_folder.split('_')[-1]
+        backup_date = datetime.strptime(backup_date_str, '%Y%m%d')
 
-        log_statement = f'Folder {backup_folder} - MT {modified_time} - TSC {time_since_modified_in_days} days'
+        todays_date = datetime.now()
+
+        days_since_created = (todays_date - backup_date).days
+
+        log_statement = f'Time {todays_date} - Folder {backup_folder} - Backup Date {backup_date} - Days since backup {days_since_created} days'
         with open(LOG_FILE, 'a', encoding='utf-8') as f:
-            f.write(log_statement)
+            f.write(log_statement+'\n')
 
         print(log_statement)
 
-        if time_since_modified_in_days > N_DAYS:
+        if days_since_created > N_DAYS:
             os.system(f'rm -r {BACKUP_PATH}{backup_folder}/*')
